75485a681430e6669ac678825e3f09539158d8a7
Ramon Silva Barros <ramon@dcfdci.fdci.br>
2024-07-08 13:40:50 -0300

    feat: adicionando o calculo do imc

--- src/components/Formulario/index.jsx
+++ src/components/Formulario/index.jsx
@@ -3,29 +3,22 @@ import { useEffect, useState } from "react";
 const Formulario = () => {
     const [altura, setAltura] = useState(0);
     const [peso, setPeso] = useState(0);
+    const [resultadoIMC, setResultadoIMC] = useState('');
 
-    
-    useEffect(() => {
-        const imc = peso / (altura*altura);
-        console.log(imc);
-        
-    }, [altura, peso]);
-    
     const resultado = () => {
-
-
-        return (
-            <>
-            <span></span>
-            </>
-        )
+        const imc = peso / (altura*altura);
+        setResultadoIMC(`Seu IMC é ${imc.toFixed(2)}`);
     }
 
     return (
 
         <div>
-            <input className="me-5" type="number" placeholder="Digite sua Altura" onChange={({target}) => setAltura(parseInt(target.value))} />
-            <input type="number" placeholder="Digite seu Peso" onChange={({target}) => setPeso(parseInt(target.value))} />
+            <input type="number" placeholder="Digite sua Altura" onChange={({target}) => setAltura(parseFloat(target.value))} />
+            <input type="number" placeholder="Digite seu Peso" onChange={({target}) => setPeso(parseFloat(target.value))} />
+            <button onClick={ resultado }>Calcular</button>
+            <div>
+                { resultadoIMC && <span> { resultadoIMC } </span>}
+            </div>
         </div>
     )
 }
