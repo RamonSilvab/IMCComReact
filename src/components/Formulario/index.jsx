2e1cbc93b93d3f859bb83c0b6bc65326406490ec
Ramon Silva Barros <ramon@dcfdci.fdci.br>
2024-07-08 14:19:24 -0300

    feat: adicionando feedback para o usuario

--- src/components/Formulario/index.jsx
+++ src/components/Formulario/index.jsx
@@ -6,20 +6,32 @@ const Formulario = () => {
     const [resultadoIMC, setResultadoIMC] = useState('');
 
     const resultado = () => {
-        const imc = peso / (altura*altura);
-        setResultadoIMC(`Seu IMC é ${imc.toFixed(2)}`);
+
+        if (altura === 0 || peso === 0) {
+            setResultadoIMC(`Os dois campos precisam ser preenchidos`);
+        } else {
+            const imc = peso / (altura * altura);
+            if (imc < 18.5) {
+                setResultadoIMC(`Seu IMC é ${imc.toFixed(1)}, você está abaixo do peso.`);
+            } else if (imc < 24.9) {
+                setResultadoIMC(`Seu IMC é ${imc.toFixed(1)}, seu peso está normal.`);
+            } else {
+                setResultadoIMC(`Seu IMC é ${imc.toFixed(1)}, você está acima do peso.`);
+            }
+        }
     }
 
     return (
-
-        <div>
-            <input type="number" placeholder="Digite sua Altura" onChange={({target}) => setAltura(parseFloat(target.value))} />
-            <input type="number" placeholder="Digite seu Peso" onChange={({target}) => setPeso(parseFloat(target.value))} />
-            <button onClick={ resultado }>Calcular</button>
+        <>
             <div>
-                { resultadoIMC && <span> { resultadoIMC } </span>}
+                <input type="number" placeholder="Digite sua Altura (m)" required onChange={({ target }) => setAltura(parseFloat(target.value))} />
+                <input type="number" placeholder="Digite seu Peso (kg)" required onChange={({ target }) => setPeso(parseFloat(target.value))} />
+                <button onClick={resultado}>Calcular</button>
             </div>
-        </div>
+            <div>
+                {resultadoIMC && <span> {resultadoIMC} </span>}
+            </div>
+        </>
     )
 }
 
